c1e5054b18f8688b7849d6bb703b2a5fd59c5ff4
ParitoshBh <mail@paritoshbh.me>
2022-09-13 19:46:22 -0400

    Remove dead code from header and show add account button on mobile

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -12,7 +12,7 @@
                 </div>
                 <div class="col-12 col-md-auto ms-auto d-print-none">
                     <div class="btn-list">
-                        <a href="{{ route('add-account') }}" class="btn btn-primary d-none d-sm-inline-block">
+                        <a href="{{ route('add-account') }}" class="btn btn-primary d-sm-inline-block">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                                  viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                                  stroke-linecap="round" stroke-linejoin="round">
