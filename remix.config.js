161a464108d59f146270558f31a85990640e5211
ParitoshBh <mail@paritoshbh.me>
2023-03-10 01:23:34 +0000

    Add postgres module as server dependency

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: ["postgres"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
