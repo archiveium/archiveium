39b42a47c2a8dce0db9e18f54f02b70fc61f7d65
ParitoshBh <mail@paritoshbh.me>
2023-05-14 20:17:35 +0000

    Start integrating vitest

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitest/config';
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+          "~": resolve(__dirname, "./app")
+        }
+      },
+    test: {
+        globals: true,
+        setupFiles: ["./test/setup-test-env.ts"],
+        include: ["./test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        exclude: ["build", "node_modules", "public"],
+        watchExclude: [
+            ".*\\/node_modules\\/.*",
+            ".*\\/build\\/.*",
+        ],
+        coverage: {
+            all: true,
+            exclude: ["build", "node_modules", "public", "test"],
+            include: ["app"],
+            reporter: ["text", "json", "html"]
+        }
+    },
+})
