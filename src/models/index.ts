918d6acb0a9390c53be8fd4757df734f8f7c63fc
ParitoshBh <--global>
2023-07-06 20:24:31 +0000

    Fix docker build

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,5 +6,7 @@ import type { DatabaseConfig, RedisConfig } from '../types/config';
 const dbConfig = config.get<DatabaseConfig>('database');
 const redisConfig = config.get<RedisConfig>('redis');
 
+// TODO These get triggered during build time resulting in errors
+// TODO Add a connection timeout for docker container to die instead of retrying forever
 export const redis = new Redis(redisConfig);
 export const sql = postgres({ ...dbConfig });
