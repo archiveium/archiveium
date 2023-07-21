e36d99cc6f2817076f77c845c3c5a145cc907f66
ParitoshBh <--global>
2023-07-21 22:05:10 +0000

    Migrate to flowbite starting with dashboard

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Navbar from '../components/navbar.svelte';
+	import '../app.postcss';
+</script>
+
+<div class="antialiased">
+	<!-- Navbar -->
+	<Navbar />
+
+	<!-- Content -->
+	<main class="p-4 h-auto pt-20">
+		<slot />
+	</main>
+</div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,99 +1,79 @@
 <script lang="ts">
-	import Header from '../../components/header.svelte';
+	import { A, Alert } from 'flowbite-svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<Header />
+<!-- Success alert -->
+{#if data.flashMessage}
+	<Alert border color="green">
+		<svg
+			slot="icon"
+			aria-hidden="true"
+			class="w-5 h-5"
+			fill="currentColor"
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill-rule="evenodd"
+				d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+				clip-rule="evenodd"
+			/></svg
+		>
+		<span class="font-medium">Success alert!</span> Change a few things up and try submitting again.
+	</Alert>
+{/if}
 
-<div class="page-body">
-	<div class="container-xl">
-		<!-- Success alert -->
-		{#if data.flashMessage}
-			<div class="alert alert-success" role="alert">
-				<div class="text-muted">{data.flashMessage.message}</div>
-			</div>
-		{/if}
-
-		<div class="card">
-			<div class="card-header">
-				<h3 class="card-title">Overview</h3>
-				<div class="card-actions">
-					<a href="/accounts/add" class="btn btn-primary btn-md">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="icon"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							stroke-width="2"
-							stroke="currentColor"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 5l0 14" /><path
-								d="M5 12l14 0"
-							/></svg
-						>
-						Add Account
-					</a>
-				</div>
-			</div>
-			<div class="card-body">
-				<div class="datagrid">
-					<div class="datagrid-item">
-						<div class="datagrid-title">Accounts</div>
-						<div class="datagrid-content">
-							{#await data.streamed.accounts.added}
-								Loading...
-							{:then value}
-								<a href="/accounts">{value}</a>
-							{/await}
-						</div>
-					</div>
-					<div class="datagrid-item">
-						<div class="datagrid-title">Syncing</div>
-						<div class="datagrid-content">
-							{#await data.streamed.accounts.syncing}
-								Loading...
-							{:then value}
-								{value}
-							{/await}
-						</div>
-					</div>
-					<div class="datagrid-item">
-						<div class="datagrid-title">Saved Emails</div>
-						<div class="datagrid-content">
-							{#await data.streamed.emails.processed}
-								Loading...
-							{:then value}
-								{value}
-							{/await}
-						</div>
-					</div>
-					<div class="datagrid-item">
-						<div class="datagrid-title">Failures</div>
-						<div class="datagrid-content">
-							{#await data.streamed.emails.failure}
-								Loading...
-							{:then value}
-								{value}
-							{/await}
-						</div>
-					</div>
-					<div class="datagrid-item">
-						<div class="datagrid-title">Quota Used</div>
-						<div class="datagrid-content">
-							{#await data.streamed.emails.processed}
-								Loading...
-							{:then value}
-								{((value / data.streamed.emails.quota) * 100).toFixed(2) + ' %'}
-							{/await}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4 mt-4">
+	<div class="flex flex-col items-center justify-center">
+		{#await data.streamed.accounts.added}
+			Loading...
+		{:then value}
+			<A class="hover:underline" href="/accounts">
+				<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{value}</dt>
+			</A>
+		{/await}
+		<dd class="font-light text-gray-500 dark:text-gray-400">accounts</dd>
+	</div>
+	<div class="flex flex-col items-center justify-center">
+		<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
+			{#await data.streamed.accounts.syncing}
+				Loading...
+			{:then value}
+				{value}
+			{/await}
+		</dt>
+		<dd class="font-light text-gray-500 dark:text-gray-400">syncing</dd>
+	</div>
+	<div class="flex flex-col items-center justify-center">
+		<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
+			{#await data.streamed.emails.processed}
+				Loading...
+			{:then value}
+				{value}
+			{/await}
+		</dt>
+		<dd class="font-light text-gray-500 dark:text-gray-400">saved emails</dd>
+	</div>
+	<div class="flex flex-col items-center justify-center">
+		<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
+			{#await data.streamed.emails.failure}
+				Loading...
+			{:then value}
+				{value}
+			{/await}
+		</dt>
+		<dd class="font-light text-gray-500 dark:text-gray-400">failures</dd>
+	</div>
+	<div class="flex flex-col items-center justify-center">
+		<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
+			{#await data.streamed.emails.processed}
+				Loading...
+			{:then value}
+				{((value / data.streamed.emails.quota) * 100).toFixed(2) + ' %'}
+			{/await}
+		</dt>
+		<dd class="font-light text-gray-500 dark:text-gray-400">quota used</dd>
 	</div>
 </div>
