d434ac9a72c7c96a22669b901b69f02322c3ea6b
ParitoshBh <--global>
2023-08-02 16:35:55 +0000

    Update dashboard view

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { A, Alert } from 'flowbite-svelte';
+	import { A, Alert, Card, Button, List, Li } from 'flowbite-svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -12,55 +12,93 @@
 	</Alert>
 {/if}
 
-<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4 mt-4">
-	<div class="flex flex-col items-center justify-center">
-		{#await data.streamed.accounts.added}
-			Loading...
-		{:then value}
-			<A class="hover:underline" href="/accounts">
-				<dt class="mb-2 text-3xl md:text-4xl font-extrabold">{value}</dt>
-			</A>
-		{/await}
-		<dd class="font-light text-gray-500 dark:text-gray-400">accounts</dd>
-	</div>
-	<div class="flex flex-col items-center justify-center">
-		<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
-			{#await data.streamed.accounts.syncing}
-				Loading...
-			{:then value}
-				{value}
-			{/await}
-		</dt>
-		<dd class="font-light text-gray-500 dark:text-gray-400">syncing</dd>
-	</div>
-	<div class="flex flex-col items-center justify-center">
-		<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
-			{#await data.streamed.emails.processed}
-				Loading...
-			{:then value}
-				{value}
-			{/await}
-		</dt>
-		<dd class="font-light text-gray-500 dark:text-gray-400">saved emails</dd>
-	</div>
-	<div class="flex flex-col items-center justify-center">
-		<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
-			{#await data.streamed.emails.failure}
-				Loading...
-			{:then value}
-				{value}
-			{/await}
-		</dt>
-		<dd class="font-light text-gray-500 dark:text-gray-400">failures</dd>
-	</div>
-	<div class="flex flex-col items-center justify-center">
-		<dt class="mb-2 text-3xl md:text-4xl font-extrabold">
-			{#await data.streamed.emails.processed}
-				Loading...
-			{:then value}
-				{((value / data.streamed.emails.quota) * 100).toFixed(2) + ' %'}
-			{/await}
-		</dt>
-		<dd class="font-light text-gray-500 dark:text-gray-400">quota used</dd>
-	</div>
+<div class="grid grid-cols-2 gap-6">
+	<Card class="max-w-none">
+		<div class="flex justify-between items-center mb-4">
+			<h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Overview</h5>
+		</div>
+		<List tag="ul" list="none" class="max-w-none divide-y divide-gray-200 dark:divide-gray-700">
+			<Li class="pb-3 sm:pb-4 pt-2">
+				<div class="flex items-center space-x-4">
+					<A href="/accounts" aClass="inline-flex w-full hover:underline">
+					<div class="flex-1 min-w-0">
+						<p class="text-sm font-medium truncate text-primary-600 dark:text-primary-500">Accounts</p>
+					</div>
+					<div class="inline-flex items-center text-base font-semibold text-primary-600 dark:text-primary-500">
+						{#await data.streamed.accounts.added}
+							Loading...
+						{:then value}
+							{value}
+						{/await}
+					</div>
+					</A>
+				</div>
+			</Li>
+			<Li class="pb-3 sm:pb-4 pt-4">
+				<div class="flex items-center space-x-4">
+					<div class="flex-1 min-w-0">
+						<p class="text-sm font-medium text-gray-900 truncate dark:text-white">Saved Emails</p>
+					</div>
+					<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+						{#await data.streamed.emails.processed}
+							Loading...
+						{:then value}
+							{value}
+						{/await}
+					</div>
+				</div>
+			</Li>
+			<Li class="pb-3 sm:pb-4 pt-4">
+				<div class="flex items-center space-x-4">
+					<div class="flex-1 min-w-0">
+						<p class="text-sm font-medium text-gray-900 truncate dark:text-white">Syncing</p>
+					</div>
+					<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+						{#await data.streamed.accounts.syncing}
+							Loading...
+						{:then value}
+							{value}
+						{/await}
+					</div>
+				</div>
+			</Li>
+			<Li class="pb-3 sm:pb-4 pt-4">
+				<div class="flex items-center space-x-4">
+					<div class="flex-1 min-w-0">
+						<p class="text-sm font-medium text-gray-900 truncate dark:text-white">Failures</p>
+					</div>
+					<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+						{#await data.streamed.emails.failure}
+							Loading...
+						{:then value}
+							{value}
+						{/await}
+					</div>
+				</div>
+			</Li>
+			<Li class="pb-3 sm:pb-4 pt-4">
+				<div class="flex items-center space-x-4">
+					<div class="flex-1 min-w-0">
+						<p class="text-sm font-medium text-gray-900 truncate dark:text-white">Quote Used</p>
+						<p class="text-sm text-gray-500 truncate dark:text-gray-400">email@flowbite.com</p>
+					</div>
+					<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+						{#await data.streamed.emails.processed}
+							Loading...
+						{:then value}
+							{((value / data.streamed.emails.quota) * 100).toFixed(2) + ' %'}
+						{/await}
+					</div>
+				</div>
+			</Li>
+		</List>
+	</Card>
+	<Card class="max-w-none items-center justify-center">
+		<div class="flex flex-col pb-4">
+			<Button href="/accounts/add">
+				Add Account
+				<svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+			</Button>
+		</div>
+	</Card>
 </div>
