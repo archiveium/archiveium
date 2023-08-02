75e4b1b36acdab116499ff5c44cef5740bf44422
ParitoshBh <--global>
2023-08-02 20:33:18 +0000

    Show email quota count

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -80,7 +80,7 @@
 				<div class="flex items-center space-x-4">
 					<div class="flex-1 min-w-0">
 						<p class="text-sm font-medium text-gray-900 truncate dark:text-white">Quote Used</p>
-						<p class="text-sm text-gray-500 truncate dark:text-gray-400">email@flowbite.com</p>
+						<p class="text-sm text-gray-500 truncate dark:text-gray-400">Maximum allowed emails {data.streamed.emails.quota}</p>
 					</div>
 					<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
 						{#await data.streamed.emails.processed}
