3bb58ac297b85611dce128af8517c8d1bc62d045
ParitoshBh <--global>
2023-08-17 16:51:14 +0000

    Fix import in email view

--- src/routes/email/[id]/+page.server.ts
+++ src/routes/email/[id]/+page.server.ts
@@ -1,11 +1,11 @@
-import { GetEmailWithS3DataByIdAndUserId } from '../../../actions/email.js';
+import * as s3Service from '$lib/server/services/s3Service';
 import { requireUserId } from '../../../utils/auth.js';
 
 export const load = async ({ locals, params }) => {
 	const userId = requireUserId(false, locals.user);
 	// TODO Validate that params.id is a number
 	// redirect otherwise with flash message
-	const email = await GetEmailWithS3DataByIdAndUserId(userId, params.id);
+	const email = await s3Service.findEmailByIdAndUserId(userId, params.id);
 
 	return { email };
 };
