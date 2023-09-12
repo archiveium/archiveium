41d7fb075e1b1490f01aa7fd6026cf51d8a68c89
ParitoshBh <--global>
2023-09-12 20:22:53 +0000

    Improve accounts view table and fix typo in dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -79,7 +79,7 @@
 			<Li class="pb-3 sm:pb-4 pt-4">
 				<div class="flex items-center space-x-4">
 					<div class="flex-1 min-w-0">
-						<p class="text-sm font-medium text-gray-900 truncate dark:text-white">Quote Used</p>
+						<p class="text-sm font-medium text-gray-900 truncate dark:text-white">Storage Used</p>
 						<p class="text-sm text-gray-500 truncate dark:text-gray-400">Maximum allowed emails {data.streamed.emails.quota}</p>
 					</div>
 					<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
