752f04dfe4be6234809e10983944f28980e9a7b8
ParitoshBh <--global>
2023-10-18 02:45:03 +0000

    Start migrating to sveltekit module for loading private env

--- src/lib/server/database/connection.ts
+++ src/lib/server/database/connection.ts
@@ -3,6 +3,7 @@ import pg from 'pg';
 import config from 'config';
 import { Kysely, PostgresDialect } from 'kysely';
 import type { DatabaseConfig } from '../../../types/config';
+import { LOG_LEVEL } from '$env/static/private';
 
 const { Pool } = pg;
 const dbConfig = config.get<DatabaseConfig>('database');
@@ -16,4 +17,10 @@ const dialect = new PostgresDialect({
 // to communicate with your database.
 export const db = new Kysely<DB>({
   dialect,
+  log(event) {
+    if (event.level === 'query' && LOG_LEVEL === 'VERBOSE') {
+      console.log(event.query.sql)
+      console.log(event.query.parameters)
+    }
+  }
 })
