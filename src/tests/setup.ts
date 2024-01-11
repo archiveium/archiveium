d38363f53fef8d1fc82a0c11357f3f3c0ebcb715
paritoshbh <--global>
2024-01-10 22:10:06 -0500

    Add tests for logout function in auth service

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,2 +1,29 @@
-// TODO Add test related global setup
 // import { vitest } from 'vitest';
+
+export class MockCookie {
+    private cookieData;
+
+    constructor(cookieData = {}) {
+        this.cookieData = cookieData;
+    }
+
+    set(key: string) {
+        // Do nothing
+    }
+
+    get(key: string) {
+        return '';
+    }
+
+    getAll() {
+        return [];
+    }
+
+    delete(key: string) {
+        // return '';
+    }    
+
+    serialize () {
+        return '';
+    }
+}
