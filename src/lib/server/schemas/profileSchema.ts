ba5a39d1c94b0f1f2778dd340dcdffdf4c6dea47
paritoshbh <--global>
2024-02-15 12:49:43 -0500

    Add profile page for changing password

---
+++ src/lib/server/schemas/profileSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const passwordUpdateFormSchema = z
+	.object({
+		currentPassword: z.string().trim().min(8, 'Must be of at-least 8 characters'),
+		password: z.string().trim().min(8, 'Must be of at-least 8 characters'),
+		passwordConfirm: z.string().trim()
+	})
+	.refine((data) => data.password === data.passwordConfirm, {
+		message: 'Passwords do not match',
+		path: ['passwordConfirm']
+	});
