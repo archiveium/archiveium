451ae69b97e7f20a939952e39dfdfaf42ed01f8c
paritoshbh <--global>
2024-02-15 16:38:27 -0500

    Add integer matcher for routes

---
+++ src/tests/unit/params/integer.spec.ts
@@ -0,0 +1,19 @@
+import { expect, describe, it } from 'vitest';
+import { match } from '../../../params/integer';
+
+describe('params', () => {
+	describe('integer matcher', () => {
+		it('should return true for integer value', async () => {
+			// act & assert
+			expect(match('1')).toBe(true);
+            expect(match('11')).toBe(true);
+		});
+
+		it('should return false for non-integer value', async () => {
+			// act & assert
+			expect(match('1s')).toBe(false);
+            expect(match('ss')).toBe(false);
+            expect(match('s1')).toBe(false);
+		});
+	});
+});
