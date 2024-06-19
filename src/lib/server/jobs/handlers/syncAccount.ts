4e0ed674f4a583dc6fb14d8950226f87be409db6
Paritosh Bhatia <mail@paritoshbh.me>
2024-06-19 02:00:19 +0000

    Add maximum execution time for syncAccount

--- src/lib/server/jobs/handlers/syncAccount.ts
+++ src/lib/server/jobs/handlers/syncAccount.ts
@@ -27,6 +27,9 @@ export async function syncAccount(job: Job): Promise<void> {
 	jobName = job.name;
 	logger.info(`${jobName}: Running job`);
 
+	// set max execution time of 10 minutes
+	setTimeout(() => new Error('TimeoutError'), 10 * 60 * 1000);
+
 	let imapClient: ImapFlow;
 	const allSyncingAccounts = await accountService.findAllSyncingAccounts();
 	for (const syncingAccount of allSyncingAccounts) {
