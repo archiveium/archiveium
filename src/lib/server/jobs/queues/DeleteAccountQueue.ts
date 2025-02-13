3ae61e82d9adb2b41d3054c44f78bbe346d51d82
ParitoshBh <mail@paritoshbh.me>
2025-02-12 20:02:05 -0500

    Use base handler for delete account job

---
+++ src/lib/server/jobs/queues/DeleteAccountQueue.ts
@@ -0,0 +1,23 @@
+import type { Job, Processor } from 'bullmq';
+import { BaseQueue } from './BaseQueue';
+import { DeleteAccountHandler } from '../handlers/DeleteAccountHandler';
+
+export class deleteAccountQueue extends BaseQueue {
+	private name = 'DeleteAccount';
+	private jobName = 'deleteAccount';
+
+	getName(): string {
+		return this.name;
+	}
+
+	getJobName(): string {
+		return this.jobName;
+	}
+
+	getProcessor(): Processor {
+		return async function (job: Job) {
+			const handler = new DeleteAccountHandler(job);
+			await handler.execute();
+		};
+	}
+}
