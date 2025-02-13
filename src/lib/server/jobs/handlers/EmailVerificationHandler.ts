edc98c7af2773d4db94acce1ccc5e7cc5e582c51
ParitoshBh <mail@paritoshbh.me>
2025-02-12 20:07:44 -0500

    Use base handler for email verification job

---
+++ src/lib/server/jobs/handlers/EmailVerificationHandler.ts
@@ -0,0 +1,15 @@
+import { BaseHandler } from "./BaseHandler";
+import * as userService from '$lib/server/services/userService';
+import { logger } from "../../../../utils/logger";
+
+export class EmailVerificationHandler extends BaseHandler {
+    async handle(): Promise<void> {
+        const allUnverifiedUsers = await userService.findUnverifiedUsers();
+        for (const unverifiedUser of allUnverifiedUsers) {
+            logger.info(`${this.jobName}: Processing user ${unverifiedUser.email}`);
+            await userService.sendUserVerificationEmail(unverifiedUser.email, unverifiedUser.id);
+            await userService.setUserNotificationDate(unverifiedUser.id, 'NOW()');
+            logger.info(`${this.jobName}: Finished processing user ${unverifiedUser.email}`);
+        }
+    }
+}
