117482eb059a47b9cf552e4282a21c8316f7bfa2
ParitoshBh <mail@paritoshbh.me>
2025-02-12 22:15:52 -0500

    Add script for populating dev database with fake emails

---
+++ scripts/populateFakeEmailData.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+import { faker } from '@faker-js/faker';
+import { mailTransporter } from '../src/lib/mailTransport/index';
+
+let fakeEmailsToGenerate = 100;
+
+async function sendFakeEmail() {
+    const fakeEmailFrom = `"${faker.internet.displayName()}" <${faker.internet.email()}>`;
+    const fakeEmailTo = 'dev.test@local.dev';
+    
+    await mailTransporter.sendMail({
+        from: fakeEmailFrom,
+        to: fakeEmailTo,
+        subject: faker.food.dish(),
+        html: faker.food.description(),
+    });
+}
+
+while (fakeEmailsToGenerate > 0) {
+    await sendFakeEmail();
+    fakeEmailsToGenerate = fakeEmailsToGenerate - 1;
+}
