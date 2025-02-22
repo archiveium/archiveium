751a19fac2f621cf80a89621b5c728408350c08d
ParitoshBh <mail@paritoshbh.me>
2025-02-21 22:49:53 -0500

    Fix aws sdk config mutation error

--- src/lib/server/s3/connection.ts
+++ src/lib/server/s3/connection.ts
@@ -3,7 +3,10 @@ import config from 'config';
 import type { S3Config } from '../../../types/config';
 import { logger } from '../../../utils/logger';
 
-const s3Config = config.get<S3Config>('s3');
+// We are doing this stringify/parse because starting v3.667.0 of aws js sdk
+// AWS SDK started mutating config object internally for credential management
+// $source is what is being mutated
+const s3Config: S3Config = JSON.parse(JSON.stringify(config.get('s3')));
 let s3Client: S3Client;
 
 try {
